6b2b3532341986bfb2d273aa21e0750b2a611657
Yash Shah <mail@yashshah.com>
2019-10-19 16:41:19 -0400

    Add token

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var actionsRouter = require("./routes/actions");
 
 var app = express();
 
@@ -16,5 +17,9 @@ app.use(cookieParser());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/actions", actionsRouter);
+
+global.token =
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvcmdJZCI6ImEyZmE3NDg1LTViMWUtNDBiNC1iZTVhLWM0ZmY2NGE2ODY3NSIsImV4cCI6MTU3MTUxNzIxNCwiaWF0IjoxNTcxNTEzNjE0fQ.JdTjgptvJbxiTHBDITylaxEfuGSMdOUL2pkPfxI8dRU";
 
 module.exports = app;
