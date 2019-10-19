fc4b49c5201910c3725cfbde0600a0095083d6ac
Ian McCausland <imccausl@gmail.com>
2019-10-19 18:16:08 -0400

    Filter out responses below the confidence threshold

--- helpers/search.js
+++ helpers/search.js
@@ -1,15 +1,30 @@
 require("dotenv").config();
-const request = require("request");
-const { API } = require("../helpers/constants");
+const _request = require("request");
+const { promisify } = require("util");
+
+const request = promisify(_request);
+const { API, CONFIDENCE_THRESHOLD } = require("../helpers/constants");
 
 const { ORG_ID } = process.env;
 
+// for testing without app.js
 if (!global.token) {
   global.token =
-    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvcmdJZCI6ImEyZmE3NDg1LTViMWUtNDBiNC1iZTVhLWM0ZmY2NGE2ODY3NSIsImV4cCI6MTU3MTUyMTgzNCwiaWF0IjoxNTcxNTE4MjM0fQ.OGrzUgUgulu-rZj0zOMnQSw4JwVngulcG_FgU-xyOpE";
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvcmdJZCI6ImEyZmE3NDg1LTViMWUtNDBiNC1iZTVhLWM0ZmY2NGE2ODY3NSIsImV4cCI6MTU3MTUyNjQzMiwiaWF0IjoxNTcxNTIyODMyfQ.n4ucuhTQLrpEx6UIG7t0REploi7kZy6874vU7GDPRts";
 }
 
-function askKnowledgeBase(kbId, query) {
+function _filterByConfidence(result) {
+  if (result && result.confidence && result.confidence > CONFIDENCE_THRESHOLD) {
+    return { result };
+  }
+
+  return {
+    KnowledgeBase: result && result.KnowledgeBase ? result.KnowledgeBase : null,
+    faq: "I'm sorry, I didn't understand that. Can you try a different wording?"
+  };
+}
+
+async function askKnowledgeBase(kbId, query) {
   const options = {
     method: "POST",
     url: `${API.root}${API.searchKnowledgeBase(kbId)}`,
@@ -30,17 +45,28 @@ function askKnowledgeBase(kbId, query) {
     json: true
   };
 
-  request(options, function(error, response, body) {
-    if (error) throw new Error(error);
-    const { KnowledgeBase, faq } = body.results[0];
-
-    console.log(KnowledgeBase, faq);
-  });
+  try {
+    const result = await request(options);
+    if (result.body && !result.body.error) {
+      return {
+        query: result.body.query,
+        result: _filterByConfidence(result.body.results[0])
+      };
+    }
+  } catch (e) {
+    return { status: "error", message: e };
+  }
 }
 
-askKnowledgeBase(
-  "1995df85-0984-4683-baa5-dc87a94b183d",
-  "My computer won't turn on! What do I do?"
-);
+const ask = async () => {
+  const faq = await askKnowledgeBase(
+    "1995df85-0984-4683-baa5-dc87a94b183d",
+    "How do I go on vacation?"
+  );
 
-module.exports = { askKnowledgeBase, findKnowledgeBase };
+  console.log(faq);
+};
+
+ask();
+
+module.exports = { askKnowledgeBase };
