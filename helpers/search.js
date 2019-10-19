90d71831816d18dfb1353afbfd31b9c60cfb3ad3
Ian McCausland <imccausl@gmail.com>
2019-10-19 17:44:46 -0400

    Add ask function

--- helpers/search.js
+++ helpers/search.js
@@ -1,32 +1,46 @@
-require('dotenv').config();
+require("dotenv").config();
+const request = require("request");
+const { API } = require("../helpers/constants");
 
-var request = require("request");
+const { ORG_ID } = process.env;
 
-var options = {
-  method: "POST",
-  url:
-    "https://api.genesysappliedresearch.com/v2/knowledge/knowledgebases//search",
-  headers: {
-    "Postman-Token": "997a9125-d3b6-4256-9d5c-e2db99773b86",
-    "cache-control": "no-cache",
-    token: global.token,
-    organizationid: ,
-    "Content-Type": "application/json"
-  },
-  body: {
-    query,
-    pageSize: 5,
-    pageNumber: 1,
-    sortOrder: "string",
-    sortBy: "string",
-    languageCode: "en-US",
-    documentType: "Faq"
-  },
-  json: true
-};
+if (!global.token) {
+  global.token =
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvcmdJZCI6ImEyZmE3NDg1LTViMWUtNDBiNC1iZTVhLWM0ZmY2NGE2ODY3NSIsImV4cCI6MTU3MTUyMTgzNCwiaWF0IjoxNTcxNTE4MjM0fQ.OGrzUgUgulu-rZj0zOMnQSw4JwVngulcG_FgU-xyOpE";
+}
 
-request(options, function(error, response, body) {
-  if (error) throw new Error(error);
+function askKnowledgeBase(kbId, query) {
+  const options = {
+    method: "POST",
+    url: `${API.root}${API.searchKnowledgeBase(kbId)}`,
+    headers: {
+      token: global.token,
+      organizationid: ORG_ID,
+      "Content-Type": "application/json"
+    },
+    body: {
+      query,
+      pageSize: 5,
+      pageNumber: 1,
+      sortOrder: "string",
+      sortBy: "string",
+      languageCode: "en-US",
+      documentType: "Faq"
+    },
+    json: true
+  };
 
-  console.log(body);
-});
+  request(options, function(error, response, body) {
+    if (error) throw new Error(error);
+    const { KnowledgeBase, faq } = body.results[0];
+
+    console.log(KnowledgeBase, faq);
+  });
+}
+
+askKnowledgeBase(
+  "1995df85-0984-4683-baa5-dc87a94b183d",
+  "My computer won't turn on! What do I do?"
+);
+
+module.exports = { askKnowledgeBase, findKnowledgeBase };
