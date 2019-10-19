2f02bb783f78a26928d32033ce3ee2559715544d
Yash Shah <mail@yashshah.com>
2019-10-19 09:12:53 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
+// router.get("/", function(req, res, next) {
+//   res.render("index", { title: "Hackathon App" });
+// });
 router.get("/", function(req, res, next) {
-  res.json({ username: "yashshah" });
+  res.json({ title: "Hackathon App" });
 });
 
 module.exports = router;
