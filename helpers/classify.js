548f9a6e2525f4c4a5835fd46a8f56915c428b2a
Ian McCausland <imccausl@gmail.com>
2019-10-20 00:09:22 -0400

    Update knowledgebase helper

--- helpers/classify.js
+++ helpers/classify.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 const _request = require("request");
 const { promisify } = require("util");
-const { CLASSIFIER_API_ENDPOINT } = require("../helpers/constants");
-
+const {
+  CLASSIFIER_API_ENDPOINT,
+  KNOWLEDGE_BASES
+} = require("../helpers/constants");
+const updateKnowledgeBase = require("./updateKnowledgeBase");
 const request = promisify(_request);
+const kbId = KNOWLEDGE_BASES.finance;
 
 const classify = async text => {
   const options = {
@@ -23,8 +27,8 @@ const classify = async text => {
   console.log("DEBUG ** classification: ", classification);
   // right now we use question and statement as answer
   // other classifications include "reject" and "greeting"
-  const isRelevant =
-    classification.includes("question") || classification.includes("statement");
+  const isRelevant = true;
+  //classification.includes("question") || classification.includes("statement");
   const isQuestion = classification.includes("question");
 
   return { text, isRelevant, isQuestion };
@@ -42,7 +46,11 @@ const getQuestionAnswerPair = () => {
 
     if (classification.isRelevant && !classification.isQuestion && question) {
       answer = classification.text;
-      return { question, answer };
+      const pair = { question, answer };
+      console.log(pair);
+      const result = updateKnowledgeBase(kbId, pair);
+      console.log(result.body);
+      return result.body;
     }
   };
 };
--- helpers/constants.js
+++ helpers/constants.js
@@ -3,10 +3,21 @@ const API = {
   searchKnowledgeBase: knowledgebaseId => {
     return `knowledgebases/${knowledgebaseId}/search`;
   },
+  updateKnowledgeBase: knowledgebaseId => {
+    return `knowledgebases/${knowledgebaseId}/languages/en-US/documents/`;
+  },
   knowledgeBases: "knowledgebases"
 };
 
 const CONFIDENCE_THRESHOLD = 0.6;
 const CLASSIFIER_API_ENDPOINT = "http://localhost:5000/";
+const KNOWLEDGE_BASES = {
+  finance: "75a77b65-e85b-4d94-864d-7f7e812131eb"
+};
 
-module.exports = { API, CONFIDENCE_THRESHOLD, CLASSIFIER_API_ENDPOINT };
+module.exports = {
+  API,
+  CONFIDENCE_THRESHOLD,
+  CLASSIFIER_API_ENDPOINT,
+  KNOWLEDGE_BASES
+};
