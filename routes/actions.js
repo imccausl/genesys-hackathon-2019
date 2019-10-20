e937abebccae0eeed7d603f3a75609e4c812e30c
Yash Shah <mail@yashshah.com>
2019-10-20 00:52:26 -0400

    Update actions.js

--- routes/actions.js
+++ routes/actions.js
@@ -1,14 +1,14 @@
 var express = require("express");
 var router = express.Router();
 const { askKnowledgeBase } = require("../helpers/search");
-const { kbId } = require("../helpers/constants");
+const { KNOWLEDGE_BASES } = require("../helpers/constants");
 var { updateAnalytics } = require("../helpers/analytics");
 
 /* GET users listing. */
 router.post("/", async function(req, res, next) {
   updateAnalytics(req.body.queryResult.queryText);
   const result = await askKnowledgeBase(
-    kbId.first,
+    KNOWLEDGE_BASES.slack,
     req.body.queryResult.queryText,
     req.body.originalDetectIntentRequest.payload.data.event.channel_type ===
       "channel"
