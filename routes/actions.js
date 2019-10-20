85ab6d5311c41f5d0d9942367bac0789e1ad84d1
Yash Shah <mail@yashshah.com>
2019-10-20 09:17:36 -0400

    Update actions.js

--- routes/actions.js
+++ routes/actions.js
@@ -1,37 +1,60 @@
 var express = require("express");
 var router = express.Router();
+var url = require("url");
+
 const { askKnowledgeBase } = require("../helpers/search");
 const { KNOWLEDGE_BASES } = require("../helpers/constants");
 var { updateAnalytics } = require("../helpers/analytics");
+// Imports the Google Cloud client library
+const { Translate } = require("@google-cloud/translate");
+
+// Creates a client
+const translate = new Translate();
 
 /* GET users listing. */
 router.post("/", async function(req, res, next) {
-  if (req.body.queryResult) {
+  console.log(req.body.queryResult);
+  if (req.body.queryResult.languageCode == "ru") {
+    let [translations] = await translate.translate(
+      req.body.queryResult.queryText,
+      "en"
+    );
+    req.body.queryResult.queryText = translations;
+    console.log(translations, req.body.queryResult.queryText);
+  }
+  if (req.body.queryResult && req.body.queryResult.queryText) {
     updateAnalytics(req.body.queryResult.queryText);
   }
-  if (req.body.queryResult.queryText === "TELEPHONY_WARMUP")
-    return res.json({});
+  var url_parts = url.parse(req.url, true);
+  console.log(url_parts);
+  var query = url_parts.query;
   if (
     req.body.queryResult.action &&
     req.body.queryResult.action.includes("smalltalk")
   )
     return res.json(req.body.queryResult);
-  if (
-    req.body.queryResult.action === "input.unknown" &&
-    req.body.queryResult.originalDetectIntentRequest &&
-    req.body.queryResult.originalDetectIntentRequest.source ===
-      "GOOGLE_TELEPHONY"
-  )
-    res.json({
-      fulfillmentText: "Welcome to the future! I'm Genesys, how can I help you?"
-    });
   const result = await askKnowledgeBase(
-    KNOWLEDGE_BASES.slack,
+    KNOWLEDGE_BASES[query["kbName"]],
     req.body.queryResult.queryText,
     req.body.originalDetectIntentRequest.payload.data
       ? req.body.originalDetectIntentRequest.payload.data.event.channel_type ===
           "channel"
       : false
+  );
+  res.json(result);
+});
+
+router.post("/fbi", async function(req, res, next) {
+  if (req.body.queryResult && req.body.queryResult.queryText) {
+    updateAnalytics(req.body.queryResult.queryText);
+  }
+  const result = await askKnowledgeBase(
+    KNOWLEDGE_BASES.fbi,
+    req.body.queryResult.queryText,
+    null,
+    req.body.queryResult.outputContexts[
+      req.body.queryResult.outputContexts.length - 1
+    ].parameters.CodeName
   );
   res.json(result);
 });
