e436792fe9a8804380d37402486fdc5ab30ec77a
Yash Shah <mail@yashshah.com>
2019-10-20 00:50:00 -0400

    Update constants.js

--- helpers/constants.js
+++ helpers/constants.js
@@ -7,5 +7,13 @@ const API = {
 };
 
 const CONFIDENCE_THRESHOLD = 0.6;
-
-module.exports = { API, CONFIDENCE_THRESHOLD };
+const CONFIDENCE_LOWER_THRESHOLD = 0.3;
+const kbId = {
+  first: "1995df85-0984-4683-baa5-dc87a94b183d"
+};
+module.exports = {
+  API,
+  CONFIDENCE_THRESHOLD,
+  CONFIDENCE_LOWER_THRESHOLD,
+  kbId
+};
